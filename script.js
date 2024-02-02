c2c1b0d4d153cf0ceaa2f7c14f24554098b4dfbe
Aditya Raj <138434890+Raj-Aditya-27@users.noreply.github.com>
2024-02-02 12:53:29 +0530

    added array of objects

--- script.js
+++ script.js
@@ -1,3 +1,135 @@
+const articles = [
+    {
+        "source": {
+            "id": null,
+            "name": "Biztoc.com"
+        },
+        "author": "benzinga.com",
+        "title": "Warren Buffett's Faith On Ferrari Seems To Pay Off As Italian Automaker Races Towards $100B Valuation",
+        "description": "Warren Buffett‘s faith in Ferrari RACE seems to be paying off, with the luxury carmaker’s market value approaching $100 billion. What Happened: Despite his skepticism towards the auto industry, Buffett expressed confidence in Ferrari at Berkshire Hathaway’s s…",
+        "url": "https://biztoc.com/x/67d65683d45f7241",
+        "urlToImage": "https://c.biztoc.com/p/67d65683d45f7241/s.webp",
+        "publishedAt": "2024-02-02T06:34:07Z",
+        "content": "Warren Buffetts faith in Ferrari RACE seems to be paying off, with the luxury carmakers market value approaching $100 billion.What Happened: Despite his skepticism towards the auto industry, Buffett … [+274 chars]"
+    },
+    {
+        "source": {
+            "id": null,
+            "name": "Marketscreener.com"
+        },
+        "author": null,
+        "title": "Chinese EV Makers' Shares Rise After Better-Than-Expected January Sales",
+        "description": "(marketscreener.com) \n By Jiahui Huang \n\n\n Shares of a host of Chinese electric-vehicle makers rose in Hong Kong, helped by better-than-expected sales in a typically slow month. \n\n Geely Automobile led gains among Hang Seng Index constituents, advancing 6.1% …",
+        "url": "https://www.marketscreener.com/quote/stock/LI-AUTO-INC-110325260/news/Chinese-EV-Makers-Shares-Rise-After-Better-Than-Expected-January-Sales-45870615/",
+        "urlToImage": "https://www.marketscreener.com/images/twitter_MS_fdblanc.png",
+        "publishedAt": "2024-02-02T06:34:06Z",
+        "content": "By Jiahui Huang \r\nShares of a host of Chinese electric-vehicle makers rose in Hong Kong, helped by better-than-expected sales in a typically slow month. \r\nGeely Automobile led gains among Hang Seng I… [+1956 chars]"
+    },
+    {
+        "source": {
+            "id": null,
+            "name": "Biztoc.com"
+        },
+        "author": "qz.com",
+        "title": "In Elon Musk Tesla pay package lawsuit, lawyer cried during deposition",
+        "description": "You want your lawyer to be on your side. The more passionate they are, the better. If they’re so passionate they can’t control their emotions, though, you might be in trouble. That’s exactly what happened to Elon Musk in a recent court case. Todd Maron, forme…",
+        "url": "https://biztoc.com/x/2144e8586a8cd7f0",
+        "urlToImage": "https://c.biztoc.com/p/2144e8586a8cd7f0/og.webp",
+        "publishedAt": "2024-02-02T06:14:05Z",
+        "content": "You want your lawyer to be on your side. The more passionate they are, the better. If theyre so passionate they cant control their emotions, though, you might be in trouble. Thats exactly what happen… [+294 chars]"
+    },
+    {
+        "source": {
+            "id": null,
+            "name": "Zacjohnson.com"
+        },
+        "author": "Disclaimer and DMCA",
+        "title": "Who Owns Hummer? – Hummer Company History",
+        "description": "Hummer is a renowned brand known for its powerful pickups and SUVs. To understand the ownership details and the fascinating history of the Hummer brand, let’s take a journey back in time. In 1992, AM General, a company specializing in military vehicles, intro…",
+        "url": "https://zacjohnson.com/who-owns-hummer/",
+        "urlToImage": "https://0043d976.rocketcdn.me/wp-content/uploads/2024/02/who-owns-hummer.jpg",
+        "publishedAt": "2024-02-02T06:07:11Z",
+        "content": "Hummer is a renowned brand known for its powerful pickups and SUVs. To understand the ownership details and the fascinating history of the Hummer brand, let’s take a journey back in time.\r\nIn 1992, A… [+27976 chars]"
+    },
+    {
+        "source": {
+            "id": null,
+            "name": "Electrek.co"
+        },
+        "author": "Fred Lambert",
+        "title": "Polestar’s rare-earth supply chain investigated amid fears of Chinese export ban",
+        "description": "Polestar is the performance electric vehicle brand that spun off from Volvo. The brand has become known for its sporty design, high-performance powertrains, and limited production. The rare-earth metals used in electric vehicle motors are essential for Polesta…",
+        "url": "https://electrek.co/2024/02/01/polestars-rare-earth-supply-chain-investigated-amid-fears-of-chinese-export-ban/",
+        "urlToImage": "https://i1.wp.com/electrek.co/wp-content/uploads/sites/3/2022/11/POLESTAR-O2S-MOBILITY.png?fit=1200%2C750&quality=82&strip=all&ssl=1",
+        "publishedAt": "2024-02-02T06:00:00Z",
+        "content": "Polestar is the performance electric vehicle brand that spun off from Volvo. The brand has become known for its sporty design, high-performance powertrains, and limited production. The rare-earth meta… [+2345 chars]"
+    },
+    {
+        "source": {
+            "id": null,
+            "name": "Electrek.co"
+        },
+        "author": "Fred Lambert",
+        "title": "Tesla Ownership Experience: 'I had to sell my house to afford it, but it’s worth every penny'",
+        "description": "The Tesla ownership experience is like no other in the auto industry. While the electric automaker has its critics, many owners express an unmatched level of satisfaction and loyalty to the brand. It’s the kind of enthusiasm that prompts people to say things…",
+        "url": "https://electrek.co/2024/02/01/tesla-ownership-experience-i-had-to-sell-my-house-to-afford-it-but-its-worth-every-penny/",
+        "urlToImage": "https://i1.wp.com/electrek.co/wp-content/uploads/sites/3/2024/02/Tesla-Model-3-Michael_Bevens.jpg?fit=1600%2C900&quality=82&strip=all&ssl=1",
+        "publishedAt": "2024-02-02T05:53:00Z",
+        "content": "The Tesla ownership experience is like no other in the auto industry. While the electric automaker has its critics, many owners express an unmatched level of satisfaction and loyalty to the brand. It… [+3542 chars]"
+    },
+    {
+        "source": {
+            "id": null,
+            "name": "Marketscreener.com"
+        },
+        "author": null,
+        "title": "Here's What Elon Musk Thinks About The Idea Of Tesla Moving To Texas",
+        "description": "(marketscreener.com) \n Tesla CEO Elon Musk has made headlines by suggesting the company could move its corporate registration to Texas. \n \n He's also said it could set up a second Gigafactory in the state, and on Tuesday night he said Tesla could make a Cybertr…",
+        "url": "https://www.marketscreener.com/quote/stock/TESLA-INC-6344549/news/Here-s-What-Elon-Musk-Thinks-About-The-Idea-Of-Tesla-Moving-To-Texas-45870613/",
+        "urlToImage": "https://www.marketscreener.com/images/twitter_MS_fdblanc.png",
+        "publishedAt": "2024-02-02T05:47:04Z",
+        "content": "Tesla CEO Elon Musk has made headlines by suggesting the company could move its corporate registration to Texas. \r\nHe's also said it could set up a second Gigafactory in the state, and on Tuesday nig… [+3184 chars]"
+    },
+    {
+        "source": {
+            "id": null,
+            "name": "Biztoc.com"
+        },
+        "author": "ccn.com",
+        "title": "Elon Musk Accuses the SEC of Plotting Against Tesla",
+        "description": "Tesla CEO Elon Musk took a dig at the U.S. Securities and Exchange Commission (SEC) on Twitter on Monday. Musk accused the SEC of plotting against Tesla, but he didn’t elaborate on the nature of the alleged plot. Instead, he shared a meme showing someone speak…",
+        "url": "https://biztoc.com/x/2024/02/02/elon-musk-accuses-the-sec-of-plotting-against-tesla/",
+        "urlToImage": "https://c.biztoc.com/p/2024/02/02/elon-musk-accuses-the-sec-of-plotting-against-tesla/og.jpg",
+        "publishedAt": "2024-02-02T05:42:06Z",
+        "content": "Tesla CEO Elon Musk took a dig at the U.S. Securities and Exchange Commission (SEC) on Twitter on Monday. Musk accused the SEC of plotting against Tesla, but he didnt elaborate on the nature of the … [+2832 chars]"
+    },
+    {
+        "source": {
+            "id": null,
+            "name": "Ccn.com"
+        },
+        "author": "ccn.com",
+        "title": "Elon Musk Accuses the SEC of Plotting Against Tesla",
+        "description": "Tesla CEO Elon Musk took a dig at the U.S. Securities and Exchange Commission (SEC) on Twitter on Monday. Musk accused the SEC of plotting against Tesla, but he didn’t elaborate on the nature of the alleged plot. Instead, he shared a meme showing someone speak…",
+        "url": "https://ccn.com/2024/02/02/elon-musk-accuses-the-sec-of-plotting-against-tesla/",
+        "urlToImage": "https://c.biztoc.com/p/2024/02/02/elon-musk-accuses-the-sec-of-plotting-against-tesla/og.jpg",
+        "publishedAt": "2024-02-02T05:42:05Z",
+        "content": "Tesla CEO Elon Musk took a dig at the U.S. Securities and Exchange Commission (SEC) on Twitter on Monday. Musk accused the SEC of plotting against Tesla, but he didnt elaborate on the nature of the … [+2832 chars]"
+    },
+    {
+        "source": {
+            "id": null,
+            "name": "Fark.com"
+        },
+        "author": null,
+        "title": "Not news: Tesla hits ford. News: Tesla hits fjord. Fark: passengers rescued by floating sauna [Hero]",
+        "description": "Not news: Tesla hits ford. News: Tesla hits fjord. Fark: passengers rescued by floating sauna",
+        "url": "https://www.fark.com/comments/13139778/Not-news-Tesla-hits-ford-News-Tesla-hits-fjord-Fark-passengers-rescued-by-floating-sauna",
+        "urlToImage": "https://usrimg-full.fark.net/3/3L/fark_3Ljdv3ADVQsOmNw40lNV1q7594c.jpg?AWSAccessKeyId=JO3ELGV4BGLFW7Y3EZXN&Expires=1707109200&Signature=uKKRDoV3rEIbExHXcU6Tn1QDHW0%3D",
+        "publishedAt": "2024-02-02T01:58:10Z",
+        "content": "<li>Links are submitted by members of the Fark community.\r\n</li><li>When community members submit a link, they also write a custom headline for the story.\r\n</li><li>Other Farkers comment on the links… [+177 chars]"
+    }
+]
 function removeActiveClass() {
     const allTopics = document.querySelectorAll(".hover-topics");
     //Remove active class from all li elements
@@ -15,6 +147,7 @@ function changeColor(element) {
     inputValue.value = null;
 }
 
+//feature of search button click
 const search = document.getElementById('searchButton');
 search.addEventListener('click', () => {
     const inputValue = document.querySelector(".input-box").value.toLowerCase();
@@ -24,7 +157,7 @@ search.addEventListener('click', () => {
             fetchNews(inputValue);
         }
         catch (e) {
-            console.log("invalid input");
+            console.error("invalid input");
         }
     }
 });
@@ -47,23 +180,41 @@ function handleSearch() {
 }
 
 //Api key work
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
-const url = "https://newsapi.org/v2/everything?q=";
+// const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+// const url = "https://newsapi.org/v2/everything?q=";
 
-window.addEventListener('load', () => fetchNews("Asia"));
+// window.addEventListener('load', () => fetchNews("Asia"));
 
-async function fetchNews(query) {
+// async function fetchNews(query) {
+//     try {
+//         const response = await fetch(`${url}${query}&apikey=${API_KEY}`);
+//         const data = await response.json();
+//         console.log(data);
+//         show(data.articles);
+//     }
+//     catch (e) {
+//         console.log("Error news: ", e);
+//     }
+// }
+
+
+window.addEventListener('load', () => fetchNews());
+
+async function fetchNews() {
     try {
-        const response = await fetch(`${url}${query}&apikey=${API_KEY}`);
-        const data = await response.json();
-        show(data.articles);
+        // const response = await fetch(`${url}${query}&apikey=${API_KEY}`);
+        // const data = await response.json();
+        console.log(articles);
+        show(articles);
     }
     catch (e) {
         console.log("Error news: ", e);
     }
 }
 
+//taking only number of words within limit given
 function limitWords(text, limit) {
+    console.log("yep");
     const words = text.split(' ');
     const truncatedWords = words.slice(0, limit);
     return truncatedWords.join(' ');
@@ -144,12 +295,12 @@ function scrollToTop() {
 }
 
 //Menu button
-function showSidebar(){
+function showSidebar() {
     const sidebar = document.querySelector('.sidebar');
-    sidebar.style.display="flex";
+    sidebar.style.display = "flex";
 }
 
-function hideSidebar(){
+function hideSidebar() {
     const sidebar = document.querySelector('.sidebar');
-    sidebar.style.display="none";
+    sidebar.style.display = "none";
 }
